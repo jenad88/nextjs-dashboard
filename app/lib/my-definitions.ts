d10e93920f0e14b30254c63333664be4aabf6603
John Enad <jenad88@hotmail.com>
2024-06-29 20:16:03 -0400

    Add changes from tutorial

---
+++ app/lib/my-definitions.ts
@@ -0,0 +1,9 @@
+export type Person = {
+    name: string;
+    age: number;
+};
+
+export type Animal = {
+    name: string;
+    age: number;
+};
---
+++ app/lib/scratch1.ts
@@ -0,0 +1,11 @@
+import { Animal, Person } from './my-definitions';
+
+const people : Person[] = [
+
+];
+
+const animals: Animal[] = [
+
+];
+
+export { people, animals };
